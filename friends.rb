3ff3f64477b29d62afc7926808fb4d857d969bfc
Isaac Alter <i_alter@yahoo.com>
2022-11-02 19:28:23 -0600

    Add part 2 I guess haha endquote

--- friends.rb
+++ friends.rb
@@ -1,4 +1,8 @@
-arrays = ["Andra", "Isaac", "James", "Conner", "Kaitlyn"]
+#arrays = ["Andra", "Isaac", "James", "Conner", "Kaitlyn"]
 #p arrays[2]
 #p arrays[4]
-p arrays[0]
+#p arrays[0]
+
+places = ["Paris", "Stockholm", "Tokyo", "Colorado", "Canada"]
+
+p places.push("Mexico")
